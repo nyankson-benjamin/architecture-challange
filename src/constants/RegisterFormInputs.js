dabaa16c3d5d53e3233f0ccc8d86550f3d20a4e6
nyankson-benjamin <117347759+nyankson-benjamin@users.noreply.github.com>
2023-06-18 20:41:58 +0000

    feat: create an inputs array for the signup page

---
+++ src/constants/RegisterFormInputs.js
@@ -0,0 +1,18 @@
+export const formInputs = [
+    { id: 1, label: "Name", placeHolder: "Jonh Doe", type: "text", name: "name" },
+    {
+      id: 2,
+      label: "E-mail",
+      placeHolder: "johndoe@gmail.com",
+      type: "email",
+      name: "email",
+    },
+    {
+      id: 3,
+      label: "Password",
+      placeHolder: "********",
+      type: "password",
+      name: "password",
+    },
+  ];
+  
