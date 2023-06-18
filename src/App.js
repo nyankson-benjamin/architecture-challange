c7803f00ad1c8040c7b11e272980c400abb33a46
nyankson-benjamin <117347759+nyankson-benjamin@users.noreply.github.com>
2023-06-18 20:46:59 +0000

    update: create the logic for displaying the current page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import Register from "./pages/Register";
+import AddPhoto from "./pages/AddPhoto";
+import AddPhoto2 from "./pages/AddPhoto2";
+import InterestPageOne from "./pages/InterestPageOne";
+import InterestPageTwo from "./pages/InterestPageTwo";
+import LastPage from "./pages/LastPage";
+import Home from "./pages/Home";
+import { useContext } from "react";
+import { ComponentContext } from "./context/ComponentContext";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const { component } = useContext(ComponentContext);
+
+  let currentCompont;
+
+  switch (component) {
+    case "home":
+      currentCompont = <Home />;
+      break;
+
+    case "register":
+      currentCompont = <Register />;
+      break;
+
+    case "addPhoto":
+      currentCompont = <AddPhoto />;
+      break;
+
+    case "addPhoto2":
+      currentCompont = <AddPhoto2 />;
+      break;
+
+    case "interestPage1":
+      currentCompont = <InterestPageOne />;
+      break;
+
+    case "interestPage2":
+      currentCompont = <InterestPageTwo />;
+      break;
+
+    case "lastPage":
+      currentCompont = <LastPage />;
+      break;
+
+    default:
+      currentCompont = <Home />;
+  }
+
+  return <>{currentCompont}</>;
 }
 
 export default App;
